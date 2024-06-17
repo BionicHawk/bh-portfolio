f1c711f17c632f2650b8a01fd41d489c7d195f99
= <angel.manzo.rosas@gmail.com>
2024-06-17 02:58:54 -0600

    About page almost done

--- app/app.js
+++ app/app.js
@@ -2,7 +2,14 @@ import express from 'express';
 import ejs from 'ejs';
 import path from 'path';
 
+/**
+ * @typedef Tecnology
+ * @property {string} image
+ * @property {string} name
+ */
+
 const app = express();
+const ip = '192.168.1.142';
 const port = 8080;
 
 app.get('/', async (_, res) => {
@@ -11,7 +18,70 @@ app.get('/', async (_, res) => {
 });
 
 app.get('/about', async (_, res) => {
-    const template = await ejs.renderFile('./views/about/about.ejs');
+    const frameworks = [
+        {
+            image: '/images/frameworks/aspnet.png',
+            name: 'ASP.NET'
+        },
+        {
+            image: '/images/frameworks/react.jpeg',
+            name: 'React'
+        },
+        {
+            image: '/images/frameworks/react.jpeg',
+            name: 'React Native'
+        },
+        {
+            image: '/images/frameworks/flutter.png',
+            name: 'Flutter'
+        },
+        {
+            image: '/images/frameworks/fastapi.jpeg',
+            name: 'FastApi'
+        },
+        {
+            image: '/images/frameworks/express.jpeg',
+            name: 'Express JS'
+        }
+    ];
+    
+    /** @type Tecnology[] */
+    const programmingLanguages = [
+        {
+            image: '/images/programming-languages/csharp.png',
+            name: 'C#'
+        },
+        {
+            image: '/images/programming-languages/js.png',
+            name: 'Javascript'
+        },
+        {
+            image: '/images/programming-languages/ts.png',
+            name: 'Typescript'
+        },
+        {
+            image: '/images/programming-languages/python.jpeg',
+            name: 'Python'
+        }
+    ];
+
+    /** @type Tecnology[] */
+    const dbManagers = [
+        {
+            image: '/images/db/postgresql.png',
+            name: 'Postgresql'
+        },
+        {
+            image: '/images/db/sqlserver.jpg',
+            name: 'MS SQL Server'
+        }
+    ];
+    
+    const template = await ejs.renderFile('./views/about/about.ejs', {
+        frameworks,
+        programmingLanguages,
+        dbManagers
+    });
     res.send(template);
 });
 
@@ -27,6 +97,6 @@ app.get('/contact', async (_, res) => {
 
 app.use(express.static(path.resolve('./public')));
 
-app.listen(port, () => {
-    console.log(`Server is listening at ${port}`);
+app.listen(port, ip, () => {
+    console.log(`Server is listening at http://${ip}:${port}`);
 })
